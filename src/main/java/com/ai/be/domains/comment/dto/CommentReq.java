2edbe7911b2a843ed87d7fa58d1e16a8b720828e
hi-story4 <wnsgur1503@naver.com>
2024-11-10 12:22:54 +0900

    mvp

--- src/main/java/com/ai/be/domains/comment/dto/CommentReq.java
+++ src/main/java/com/ai/be/domains/comment/dto/CommentReq.java
@@ -13,7 +13,6 @@ public record CommentReq(
   @NotNull @Pattern(regexp = "^[0-9]{10,11}$", message = "휴대폰 번호는 10-11자리의 숫자만 입력 가능합니다" ) String phone,
   @NotNull String postId,
 
-
   @Size(min = 10, max = 500) @NotEmpty String commentContent
 )
 {}
